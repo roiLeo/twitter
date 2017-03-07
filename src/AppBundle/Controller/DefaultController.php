564c219879b895b67f5870f568b4d579b7a9db67
Nobody <medina.leo42@gmail.com>
2017-03-07 16:50:35 +0100

    test template twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,22 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/twig-test", name="app_twig_test")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render(':default:index.html.twig', array(
+            'name' => 'john',
+            'days' => array(
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+                'Sunday' ),
+            'html' => '<b> ce texte n\'est pas en gras!</b>',
+        ));
     }
 }
