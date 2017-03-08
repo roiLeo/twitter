f4fa41eec69d62c7366235a546f01d7114500b55
Nobody <medina.leo42@gmail.com>
2017-03-08 17:18:24 +0100

    parameters tweet limit

--- src/AppBundle/Controller/TweetController.php
+++ src/AppBundle/Controller/TweetController.php
@@ -16,7 +16,9 @@ class TweetController extends Controller
     public function listAction(Request $request)
     {
 
-        $tweets = $this->getDoctrine()->getRepository(Tweet::class)->getLastTweets();
+        $tweets = $this->getDoctrine()->getRepository(Tweet::class)->getLastTweets(
+            $this->getParameter('app.tweet.nb_last',10)
+        );
         return $this->render(':tweet:list.html.twig', array('tweets' => $tweets));
     }
 }
