21face366eec54fb743bd29ba66262319662d82d
Nobody <medina.leo42@gmail.com>
2017-03-09 15:03:49 +0100

    page add tweet + database + TimeAgoExtension config

--- src/AppBundle/Controller/TweetController.php
+++ src/AppBundle/Controller/TweetController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\TweetType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Tweet;
 
 
@@ -18,6 +20,27 @@ class TweetController extends Controller
             $this->getParameter('app.tweet.nb_last',10)
         );
         return $this->render(':tweet:list.html.twig', ['tweets' => $tweets]);
+    }
+
+    /**
+     * @Route("/tweet/new", name="app_tweet_new", methods={"GET","POST"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function newAction(Request $request)
+    {
+        $form = $this->createForm(TweetType::class, new Tweet());
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $tweet = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em -> persist($tweet);
+            $em -> flush();
+
+            return $this->redirectToRoute('app_tweet_view',['id' => $tweet->getId()]);
+        }
+        return $this->render(':tweet:new.html.twig', ['form' => $form->createView() ]);
     }
 
     /**
