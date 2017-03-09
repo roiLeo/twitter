84bfbbbc0705476c4d26d15a0b3e6ff9964be713
Nobody <medina.leo42@gmail.com>
2017-03-09 15:34:13 +0100

    add errors for input new tweet

--- src/AppBundle/Entity/Tweet.php
+++ src/AppBundle/Entity/Tweet.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Tweet
@@ -23,13 +24,20 @@ class Tweet
 
     /**
      * @var string
-     *
+     * @Assert\NotBlank()
+     * @Assert\Length(min=6,
+     *     max=160,
+     *     minMessage = "Your message must be at least {{ limit }} characters long",
+     *     maxMessage = "Your message cannot be longer than {{ limit }} characters"
+     * )
      * @ORM\Column(name="message", type="string", length=160)
      */
     private $message;
 
     /**
      * @var \DateTime
+     * @Assert\NotBlank()
+     * @Assert\DateTime()
      *
      * @ORM\Column(name="created_at", type="datetime")
      */
